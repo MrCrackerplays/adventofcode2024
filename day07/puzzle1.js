9dadeea528d6211b1fffe74788997d4e1a3774fe
MrCrackerplays <mrcrackerplays@gmail.com>
2024-12-07 06:33:01 +0100

    finished day 7 puzzle 1

---
+++ day07/puzzle1.js
@@ -0,0 +1,34 @@
+const fs = require("node:fs");
+const FILENAME = "input";
+
+
+(function main () {
+	//[line][result] || [line][parts][number]
+	const data = fs.readFileSync(FILENAME, "utf8").split('\n').slice(0, -1).map(l => l.split(": ").map((v, i) => {
+		if (i === 0) return +v;
+		return v.split(' ').map(n => +n);
+	}));
+	/** 
+	 * @param {number} result
+	 * @param {number} current_value
+	 * @param {number[]} remaining_values
+	 * @returns {boolean}
+	 */
+	let has_truth = function(result, current_value, remaining_values) {
+		if (remaining_values.length === 0) return current_value === result;
+		return has_truth(result, current_value + remaining_values[0], remaining_values.filter((_, i) => i !== 0))
+			|| has_truth(result, current_value * remaining_values[0], remaining_values.filter((_, i) => i !== 0));
+	}
+	let total = 0;
+	for (let line of data) {
+		/** @type(number) */
+		const result = line[0];
+		/** @type(number[]) */
+		const values = line[1];
+		if (has_truth(result, values[0], values.filter((_, i) => i !== 0))) {
+			total += result;
+		}
+	}
+
+	console.log("total number uniquely obstacles", total);
+})();
